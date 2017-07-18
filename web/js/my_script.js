21461bf3c502374bee9d765626bed0e3c155ed06
alexeevyci <alexeevyci@yahoo.com>
2017-07-18 22:17:14 +0300

    slicker

--- web/js/my_script.js
+++ web/js/my_script.js
@@ -1,17 +1,11 @@
-// var el = document.querySelector('input.choosed_color');
-// var picker = new CP(el);
-// picker.on("change", function(color) {
-//     $(el).css('background-color', '#'+color);
-//     el.value('#'+color);
-// });
-
 $(document).ready(function() {
- 	// vars
- 	var chooseColorOptions = $('select#chooseColor option');
-
-
-	chooseColorOptions.each(function() {
-		var tempColor = $(this).attr('value');
-		$(this).prepend('<div class="square_color" style="background-color:#'+tempColor+'; width:5px; height:5px"></div>');
-	})
+ 	 $('.last_product_carousel').slick({
+ 	 	infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 2,
+		dots: true,
+		arrows: true,
+		autoplay: true,
+ 		autoplaySpeed: 2000,
+ 	 });
 });
